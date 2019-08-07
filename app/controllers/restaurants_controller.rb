3ce7dc21ad44e9701abab0760f38b06744c39b80
Khoa Le <khoatle165@gmail.com>
2019-08-07 12:06:25 +0200

    add routes, add styling

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,29 @@
 class RestaurantsController < ApplicationController
+  before_action :find_article, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    new_restaurant = Restaurant.create(restaurant_strong_params)
+    redirect_to restaurant_path(new_restaurant)
+  end
+
+  private
+
+  def restaurant_strong_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id].to_i)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :restaurants do
-  #   resources :reviews
-  # end
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews
+  end
 end

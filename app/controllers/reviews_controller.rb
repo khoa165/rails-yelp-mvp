c8212852a93c34a94c0b58d27bed2387d03f9840
Khoa Le <khoatle165@gmail.com>
2019-08-07 13:11:16 +0200

    create page for new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(reviews_strong_params)
+    @review.restaurant =  @restaurant
+
+    if @review.save #return true or false
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reviews_strong_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id].to_i)
+  end
 end

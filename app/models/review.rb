a3be957165c7e22560c8285b3aa15c90aac7b005
Khoa Le <khoatle165@gmail.com>
2019-08-07 13:21:44 +0200

    fix form in review, fix style

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,10 @@
 class Review < ApplicationRecord
+  RATINGS = (0..5).to_a
+
   # Relationships
   belongs_to :restaurant
 
   # Validations
   validates :content, presence: true
-  validates :rating, numericality: true, inclusion: {in: (0..5).to_a}
+  validates :rating, numericality: true, inclusion: {in: RATINGS}
 end

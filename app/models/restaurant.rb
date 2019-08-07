a7b28d57f7cf0428b042a5c5bbb416aee447bab0
Khoa Le <khoatle165@gmail.com>
2019-08-07 11:06:12 +0200

    reset migration, add validations for restaurant and review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,12 @@
 class Restaurant < ApplicationRecord
+  # Constant
+  CUISINES = %w(chinese italian japanese french belgian)
+
+  # Relationships
   has_many :reviews, dependent: :destroy
+
+  # Validations
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: {in: CUISINES}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
+  # Relationships
   belongs_to :restaurant
+
+  # Validations
+  validates :content, presence: true
+  validates :rating, inclusion: {in: %w(0 1 2 3 4 5)}
 end
